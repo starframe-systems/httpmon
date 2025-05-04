e28b3c6b6ca83efbaf8d4c6d4a91484ab34be2ae
Brendan Berg <brendan@berg.industries>
2025-05-03 22:33:32 -0700

    Add tests for handler function (#3)

--- CanaryWorker/src/request.ts
+++ CanaryWorker/src/request.ts
@@ -3,14 +3,36 @@ import https from 'https';
 export const request = (options: https.RequestOptions, data?: any) => {
     return new Promise((resolve, reject) => {
         const req = https.request(options, (res) => {
-            res.on('end', () => {
-                resolve(res);
+            let body = '';
+
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
+
+            res.on('end', (chunk?) => {
+                if (chunk) {
+                    body += chunk;
+                }
+
+                resolve({
+                    statusCode: res.statusCode,
+                    headers: res.headers,
+                    body,
+                });
+            });
+
+            res.on('error', (err) => {
+                reject(err);
             });
         });
 
         req.on('error', (err) => {
             reject(err);
         });
+
+        // req.on('response', (res) => {
+        //     resolve(res);
+        // });
 
         if (data) {
             req.write(data);
